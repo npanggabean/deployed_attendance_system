ecc38a440bb4e1116a6064494477dee055cf57b3
npanggabean <npanggabean@student.unimelb.edu.au>
2023-11-10 17:43:05 +1100

    Fix #1

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -20,11 +20,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   // Serve the React application's index.html file if no API route is hit
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
   });
 }
 
