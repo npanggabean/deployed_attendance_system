482f54e9884597a8b061f724faaaf89a85a09069
npanggabean <npanggabean@student.unimelb.edu.au>
2023-11-10 17:36:20 +1100

    Debugging #4

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import fs from 'fs';
 import './loadEnvironment.mjs';
 import records from './routes/record.mjs';
 
@@ -17,23 +16,18 @@ app.use("/record", records);
 
 const clientBuildPath = path.join(process.cwd(), 'mern/client/build');
 
-fs.readdir(clientBuildPath, (err, files) => {
-  if (err) console.log(err);
-  else console.log("Files in build directory:", files);
-});
-
 app.use(express.static(clientBuildPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(clientBuildPath));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(clientBuildPath, 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(clientBuildPath));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(clientBuildPath, 'index.html'));
+//   });
+// }
 
 // Start the Express server
 app.listen(PORT, () => {
