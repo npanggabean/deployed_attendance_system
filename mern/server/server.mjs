dd5fd746cb85b6de5160251114972e4daeba9868
npanggabean <npanggabean@student.unimelb.edu.au>
2023-11-10 15:45:32 +1100

    Heroku Deployment #4

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -1,8 +1,8 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 import path from 'path';
-import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+import './loadEnvironment.mjs';
+import records from './routes/record.mjs';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
